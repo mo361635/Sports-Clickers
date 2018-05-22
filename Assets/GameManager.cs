d78cc10e5b4a26d3460af4792bb62ef46e3fb2e5
Oswaldo <munozbonifacioo@go.lisd.net>
2018-05-22 12:37:17 -0500

    Update

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager singleton;
+    public float m_Points;
+    public Text m_Counter;
+
+	private void Awake()
+	{
+        if (singleton != null)
+            Destroy(singleton);
+        else
+            singleton = this;
+	}
+
+	private void Update()
+	{
+        m_Counter.text = m_Points.ToString();
+	}
+
+	public void AddPoint(){
+        m_Points++;
+    }
+}
