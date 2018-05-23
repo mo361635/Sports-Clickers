1caca3f27f8e201ca5733ef392a63c43858338c3
Oswaldo <munozbonifacioo@go.lisd.net>
2018-05-23 13:26:56 -0500

    Update

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager singleton;
+    public float m_Points;
+    public Text m_Counter;
+
+    public List<GameObject> m_Icons;
+
+    public AudioSource m_BaseballHit;
+    public AudioSource m_FootballHit;
+    public AudioSource m_GolfballHit;
+    public AudioSource m_SoccerballHit;
+    public AudioSource m_BasketballSwish;
+
+    public GameObject m_ShopMenu;
+    public GameObject m_FootballMenu;
+    public GameObject m_GolfMenu;
+    public GameObject m_BasketballMenu;
+    public GameObject m_SoccerMenu;
+    public GameObject m_BaseballMenu;
+
+	private void Awake()
+	{
+        if (singleton != null)
+            Destroy(singleton);
+        else
+            singleton = this;
+
+        m_ShopMenu.SetActive(false);
+        m_FootballMenu.SetActive(false);
+        m_GolfMenu.SetActive(false);
+        m_BasketballMenu.SetActive(false);
+        m_SoccerMenu.SetActive(false);
+        m_BaseballMenu.SetActive(false);
+	}
+
+	private void Start()
+	{
+        StartCoroutine(EarnPoints());
+	}
+
+	private void Update()
+	{
+        m_Counter.text = "points : " + m_Points;
+	}
+
+	public void AddPoint(){
+        m_Points++;
+        SpawnRandomIcon();
+    }
+
+    public void SpawnRandomIcon(){
+        int index = Random.Range(0, m_Icons.Count);
+        Vector3 randomPos = new Vector3(Random.Range(-7f,7f), Random.Range(-4f,4f),0);
+        GameObject prefab = Instantiate(m_Icons[index], randomPos, Quaternion.identity);
+        if (prefab.name.Contains("football"))
+            m_FootballHit.Play();
+        if (prefab.name.Contains("baseball"))
+            m_BaseballHit.Play();
+        if (prefab.name.Contains("golfball"))
+            m_GolfballHit.Play();
+        if (prefab.name.Contains("soccerball"))
+            m_SoccerballHit.Play();
+        if (prefab.name.Contains("basketball"))
+            m_BasketballSwish.Play();
+    }
+
+    public Text v_TomBradyPriceText;
+    public Text v_TomBradyLevelText;
+    public float v_TomBradyLevel;
+    public float v_TomBradyPrice = 10;
+    public int v_TomBradyRate = 0;
+
+    public void BuyTomBrady(){
+        if(m_Points >= v_TomBradyPrice){
+            m_Points -= v_TomBradyPrice;
+            v_TomBradyPrice += 10;
+            v_TomBradyLevel++;
+            v_TomBradyRate++;
+            v_TomBradyPriceText.text = "Price : " + v_TomBradyPrice;
+            v_TomBradyLevelText.text = "Level : " + v_TomBradyLevel;
+        }
+    }
+
+    public IEnumerator EarnPoints(){
+        while(true){
+            float points = 0;
+            points += v_TomBradyRate;
+            m_Points += points;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    public void OpenShopMenu(){
+        if(m_FootballMenu.activeInHierarchy || m_GolfMenu.activeInHierarchy || m_BasketballMenu.activeInHierarchy || m_SoccerMenu.activeInHierarchy || m_BaseballMenu.activeInHierarchy)
+        {
+            m_FootballMenu.SetActive(false);
+            m_GolfMenu.SetActive(false);
+            m_BasketballMenu.SetActive(false);
+            m_SoccerMenu.SetActive(false);
+            m_BaseballMenu.SetActive(false);
+        }else{
+            m_ShopMenu.SetActive(!m_ShopMenu.activeInHierarchy);
+        }
+    }
+
+    public void OpenFootballMenu(){
+        m_FootballMenu.SetActive(true);
+        m_ShopMenu.SetActive(false);
+    }
+
+    public void OpenGolfMenu(){
+        m_GolfMenu.SetActive(true);
+        m_ShopMenu.SetActive(false);
+    }
+
+    public void OpenBasketballMenu(){
+        m_BasketballMenu.SetActive(true);
+        m_ShopMenu.SetActive(false);
+    }
+
+    public void OpenSoccerMenu(){
+        m_SoccerMenu.SetActive(true);
+        m_ShopMenu.SetActive(false);
+    }
+
+    public void OpenBaseballMenu(){
+        m_BaseballMenu.SetActive(true);
+        m_ShopMenu.SetActive(false);
+    }
+}
