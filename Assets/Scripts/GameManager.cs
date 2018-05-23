8644d266e221e38863376e28927925604072962d
Oswaldo <munozbonifacioo@go.lisd.net>
2018-05-23 13:44:58 -0500

    Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -103,6 +103,7 @@ public class GameManager : MonoBehaviour {
 			v_CamNewtonLevelText.text = "Level : " + v_CamNewtonLevel;
 		}
 	}
+
     public Text v_TigerWoodsPriceText;
     public Text v_TigerWoodsLevelText;
     public float v_TigerWoodsLevel;
@@ -140,6 +141,26 @@ public class GameManager : MonoBehaviour {
             v_JordanSpiethLevelText.text = "Level : " + v_JordanSpiethLevel;
         }
     }
+
+    public Text v_PhilMicklesonPriceText;
+    public Text v_PhilMicklesonLevelText;
+    public float v_PhilMicklesonLevel;
+    public float v_PhilMicklesonPrice = 10;
+    public int v_PhilMicklesonRate = 0;
+
+    public void BuyPhilMickleson()
+    {
+        if (m_Points >= v_PhilMicklesonPrice)
+        {
+            m_Points -= v_PhilMicklesonPrice;
+            v_PhilMicklesonPrice += 10;
+            v_PhilMicklesonLevel++;
+            v_PhilMicklesonRate++;
+            v_PhilMicklesonPriceText.text = "Price : " + v_PhilMicklesonPrice;
+            v_PhilMicklesonLevelText.text = "Level : " + v_PhilMicklesonLevel;
+        }
+    }
+
     public IEnumerator EarnPoints(){
         while(true){
             float points = 0;
